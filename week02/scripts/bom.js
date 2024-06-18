37cffd09b93e419ffd397a9f21b4e6913c30813a
sil <narradoresdigitales@gmail.com>
2024-06-18 14:54:02 -0500

    working shopping list :)

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,17 +1,47 @@
+//reference to the input
 const input = document.querySelector('#favchap');
+
+//reference to the button
 const button = document.querySelector('button');
-const list = document.querySelector('')
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value; 
-deleteButton.textContext = 'X';
-li.append(deleteButton);
-list.append(li);
+
+//reference to the list
+const list = document.querySelector('#list')
+
+
 
 //create a click event listener for the Add Chapter button
 button.addEventListener('click', function() {
-    if (input.value.trim() !== '') 
-        { 
+    if (input.value.trim() !== '') { 
+
+        //create a li element for each entry
+        const li = document.createElement('li');
+        
+        // set the li text content to the input value
+        li.textContent = input.value;
+
+        //Create a delete button
+        const deleteButton = document.createElement('button');
+
+        //Populate the button textContent with a ❌
+        deleteButton.textContent = '❌';
+        
+        //Append the li element variable with the delete button
+        li.append(deleteButton);
+
+        // Append the li element to the list
+        list.append(li)
             
-        }
-});
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+    });
+            
+        
+        
+
+    input.value = '';
+    input.focus();
+
+    }
+
+}); 
