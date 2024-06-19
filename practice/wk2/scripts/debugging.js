e1d0c7198576c68acb163aebf10ca6a7cfe1d441
sil <narradoresdigitales@gmail.com>
2024-06-19 09:10:21 -0500

    debugging js activity

--- practice/wk2/scripts/debugging.js
+++ practice/wk2/scripts/debugging.js
@@ -1,6 +1,32 @@
+// const PI = 3.14;
+// let  radius = 3;
+// let area = 0;
+
+// // view assignment to PI
+// console.log('PI = ', PI)
+// // calculate area
+// area = radius * radius * PI;
+// // display result
+// console.log('Area2: ', area)
+// // new assignment to radius
+// radius = 4;
+// //perform calculation
+// area = radius * radius * PI;
+// // view result
+// console.log('Area2: ', area);
+
+// Activity 2 // 
+
 const PI = 3.14;
-const radius = 3;
 let area = 0;
-area = radius * radius * PI;
-radius = 4;
-area = radius * radius * PI;
+
+// create a new circleArea function
+function circleArea(radius) {
+    const area = radius * PI;
+    return area;
+}
+area = circleArea(3);
+console.log('Area1: ', area);
+
+area = circleArea(4);
+console.log('Area2: ', area);
