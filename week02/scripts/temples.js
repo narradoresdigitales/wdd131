e8a826572ac8d2d5eb62679b9b24efc3ff4332ce
sil <narradoresdigitales@gmail.com>
2024-06-19 16:22:40 -0500

    more revisions

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -1,9 +1,18 @@
 
+// const hamButton = document.querySelector('#menu');
+// const navigation = document.querySelector('.nav-links');
+
+// hamButton.addEventListener('click', () => {
+//     navigation.classList.toggle('open');
+//     hamButton.classList.toggle('open');
+
+// });
+
+
 const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.nav-links');
+const navigation = document.querySelector('.navigation');
 
 hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    hamButton.classList.toggle('open');
-
-});
+	navigation.classlist.toggle('open');
+	hamButton.classList.toggle('open');
+}); 
