4477c5650b3f45fa7f83a7949240da1eb13ae807
sil <narradoresdigitales@gmail.com>
2024-07-03 11:11:06 -0500

    edits to practice js

--- practice/filtered-temples-practice.js
+++ practice/filtered-temples-practice.js
@@ -19,6 +19,7 @@ const temples = [
     location: "Aba, Nigeria",
     dedicated: "2005, August, 7",
     area: 11500,
+    region: "Africa",
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
     },
@@ -27,6 +28,7 @@ const temples = [
     location: "Manti, Utah, United States",
     dedicated: "1888, May, 21",
     area: 74792,
+    region: "North America",
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
     },
@@ -35,6 +37,7 @@ const temples = [
     location: "Payson, Utah, United States",
     dedicated: "2015, June, 7",
     area: 96630,
+    region: "North America",
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x250/payson-utah-temple-exterior-1416671-wallpaper.jpg"
     },
@@ -43,6 +46,7 @@ const temples = [
     location: "Yigo, Guam",
     dedicated: "2020, May, 2",
     area: 6861,
+    region: "Western Pacific",
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
     },
@@ -51,6 +55,7 @@ const temples = [
     location: "Kensington, Maryland, United States",
     dedicated: "1974, November, 19",
     area: 156558,
+    region: "North America",
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
     },
@@ -59,6 +64,7 @@ const temples = [
     location: "Lima, Perú",
     dedicated: "1986, January, 10",
     area: 9600,
+    region: "South America",
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
     },
@@ -67,6 +73,7 @@ const temples = [
     location: "Mexico City, Mexico",
     dedicated: "1983, December, 2",
     area: 116642,
+    region: "Mexico",
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
     },
@@ -76,6 +83,7 @@ const temples = [
     location: "Buenos Aires, Argentina",
     dedicated: "1986, January, 17",
     area: 17687,
+    region: "South America",
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/buenos-aires-argentina/400x250/buenos-airies-argentina-temple-1009069-wallpaper.jpg"
     },
@@ -84,6 +92,7 @@ const temples = [
     location: "Córdoba, Argentina",
     dedicated: "2015, May, 17",
     area: 34369,
+    region: "South America",
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/cordoba-argentina/400x250/cordoba-argentina-temples-buildings-1436938-wallpaper.jpg"
     }
@@ -96,8 +105,43 @@ const oldTemplesLink = document.querySelector(".old");
 const newTemplesLink = document.querySelector(".new");
 const largeTemplesLink = document.querySelector(".large");
 const smallTemplesLink = document.querySelector(".small");
+const southAmericaLink = document.querySelector(".southAmerica");
+const northAmericaLink = document.querySelector(".northAmerica");
+const africaLink = document.querySelector(".africa");
+const mexicoLink = document.querySelector(".mexico");
+const westernPacificLink = document.querySelector(".westernPacific");
 
 // Add event listeners for each variable/link 
+southAmericaLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    createTempleCard(temples.filter(temple => temple.region === "South America"));
+    closeMenu();
+});
+
+northAmericaLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    createTempleCard(temples.filter(temple => temple.region === "North America"));
+    closeMenu();
+});
+
+africaLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    createTempleCard(temples.filter(temple => temple.region === "Africa"));
+    closeMenu();
+});
+
+mexicoLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    createTempleCard(temples.filter(temple => temple.region === "Mexico"));
+    closeMenu();
+});
+
+westernPacificLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    createTempleCard(temples.filter(temple => temple.region === "Western Pacific"));
+    closeMenu();
+});
+
 oldTemplesLink.addEventListener("click", function(event) { 
     event.preventDefault();
     createTempleCard(temples.filter(temple => new Date (temple.dedicated) < new Date ("2000-01-01")));
@@ -137,12 +181,14 @@ function createTempleCard(filteredTemples) {
         let location = document.createElement("p");
         let dedication = document.createElement("p");
         let area  = document.createElement("p");
+        let region = document.createElement("p");
         let img = document.createElement("img");
 
         name.textContent = temple.templeName;
         location.innerHTML = `<span class="label">Location:</span> ${temple.location}`;
         dedication.innerHTML = `<span class="label">Dedicated:</span> ${temple.dedicated}`;
         area.innerHTML = `<span class="label">Size:</span> ${temple.area} sq ft`;
+        region.innerHTML = `<span class="label">Region:</span> ${temple.region}`;
         img.setAttribute("src", temple.imageUrl);
         img.setAttribute("alt", `${temple.templeName} Temple`);
         img.setAttribute("loading", "lazy");
@@ -151,6 +197,7 @@ function createTempleCard(filteredTemples) {
         card.appendChild(location);
         card.appendChild(dedication);
         card.appendChild(area);
+        card.appendChild(region);
         card.appendChild(img);
 
         document.querySelector(".res-grid").appendChild(card);
