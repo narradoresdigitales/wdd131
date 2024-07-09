0473e3b9a7d79b59ca1c5fbe098b2098798f8f8d
sil <narradoresdigitales@gmail.com>
2024-07-09 17:42:16 -0500

    updated object literals in script.

--- scripts/form.js
+++ scripts/form.js
@@ -1,27 +1,27 @@
 
 const products = [
     {
-        id: fc-1888,
+        id: "fc-1888",
         name: "flux capacitor",
         avgRating: 4.5
     },
     {
-        id: fc-2050,
+        id: "fc-2050",
         name: "power laces",
         avgRating: 4.7
     },
     {
-        id: fs-1987,
+        id: "fs-1987",
         name: "time circuits",
         avgRating: 3.5
     },
     {
-        id: ac-2000,
+        id: "ac-2000",
         name: "low voltage reactor",
         avgRating: 3.9
     },
     {
-        id: jj-1969,
+        id: "jj-1969",
         name: "warp equalizer",
         avgRating: 5.0
     }
